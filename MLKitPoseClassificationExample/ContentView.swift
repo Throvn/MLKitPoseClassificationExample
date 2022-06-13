ec08ab739a37f99691b1ea0dbfb3d5f24cf14fce
Louis <tagnacht2@gmail.com>
2022-06-13 10:21:27 +0200

    Initial Commit

---
+++ MLKitPoseClassificationExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MLKitPoseClassificationExample
+//
+//  Created by Louis Stanko on 13.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MLKitPoseClassificationExample/MLKitPoseClassificationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLKitPoseClassificationExampleApp.swift
+//  MLKitPoseClassificationExample
+//
+//  Created by Louis Stanko on 13.06.22.
+//
+
+import SwiftUI
+
+@main
+struct MLKitPoseClassificationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
